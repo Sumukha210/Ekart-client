5eacbb3abb2ed9971ef712e818784b61190f004b
sumukha kb <113685718+sumukhakb@users.noreply.github.com>
2023-06-04 13:59:18 +0530

    chore: next and tailwind config change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        yellow: "#FFC017",
-        dark: "#191919",
+        primary: "var(--primary-color)",
       },
     },
   },
