d22b09470024c5371d7f651acf2ed5eb4f2dfa3f
sumukha kb <113685718+sumukhakb@users.noreply.github.com>
2023-06-06 16:13:31 +0530

    refactor: create separate components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,44 +1,30 @@
-import SubHeading from "@/elements/SubHeading";
 import Categories from "@/layout/home/Categories";
+import ChooseUs from "@/layout/home/ChooseUs";
+import FeaturedTopRatedProduct from "@/layout/home/FeaturedTopRatedProduct";
 import HeroCarousel from "@/layout/home/HeroCarousel";
-import { IProduct } from "@/lib/types";
-import { useGetFeaturedTopRatedProductsQuery } from "@/redux/api/productApi";
-import ProductCard from "@/shared/ProductCard";
 import SectionContainer from "@/shared/SectionContainer";
-
-const FeaturedTopRatedProduct: React.FC<{ param: string }> = ({ param }) => {
-  const { data, isLoading, isError } =
-    useGetFeaturedTopRatedProductsQuery(param);
-
-  if (isError) return <></>;
-  if (isLoading) return <>Loading....</>;
-
-  return (
-    <>
-      <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-8">
-        {data?.result.map((product: IProduct) => (
-          <ProductCard {...product} key={product.id} />
-        ))}
-      </div>
-    </>
-  );
-};
 
 const Home = () => {
   return (
     <>
       <HeroCarousel />
 
+      <ChooseUs />
+
       <SectionContainer>
-        <SubHeading text="Featured Products" />
-        <FeaturedTopRatedProduct param="&featured=true" />
+        <FeaturedTopRatedProduct
+          params="&featured=true"
+          heading="Featured Products"
+        />
       </SectionContainer>
 
       <Categories />
 
       <SectionContainer>
-        <SubHeading text="Top Rated Products" />
-        <FeaturedTopRatedProduct param="&topRated=1" />
+        <FeaturedTopRatedProduct
+          params="&topRated=1"
+          heading="Top Rated Products"
+        />
       </SectionContainer>
     </>
   );
